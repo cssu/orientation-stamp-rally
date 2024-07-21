0afe43a95272bcd1b6c2b0455ea527179afe4a25
Barış <bayazitbaris@gmail.com>
2024-07-21 03:32:33 +0300

    Prepare for jenkins

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -11,16 +11,25 @@ declare global {
 
 let redis: RedisDBs
 
+const ephemeral_url =
+    process.env.NODE_ENV === 'production'
+        ? 'redis://redis_ephemeral:6379'
+        : 'redis://localhost:6379'
+const persistent_url =
+    process.env.NODE_ENV === 'production'
+        ? 'redis://redis_persistent:6379'
+        : 'redis://localhost:6380'
+
 if (process.env.NODE_ENV === 'production') {
     redis = {
-        ephemeral: new Redis('redis://localhost:6379'),
-        persistent: new Redis('redis://localhost:6380')
+        ephemeral: new Redis(ephemeral_url),
+        persistent: new Redis(persistent_url)
     }
 } else {
     if (!global.redis) {
         global.redis = {
-            ephemeral: new Redis('redis://localhost:6379'),
-            persistent: new Redis('redis://localhost:6380')
+            ephemeral: new Redis(ephemeral_url),
+            persistent: new Redis(persistent_url)
         }
     }
     redis = global.redis
