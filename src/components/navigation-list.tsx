26cd15d89d25c5f7a9c06af735a88ecac58147e3
Barış <bayazitbaris@gmail.com>
2024-09-07 04:09:56 -0400

    fix defaults

--- src/components/navigation-list.tsx
+++ src/components/navigation-list.tsx
@@ -37,7 +37,7 @@ export default async function NavigationList() {
                             </Link>
                         </div>
                     ) : (
-                        <Login />
+                        <Login size="default" />
                     )}
                 </NavigationMenuItem>
             </NavigationMenuList>
