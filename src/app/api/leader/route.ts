02dcc91bde9eb1e078eec1f4149253d0bccec232
Barış <bayazitbaris@gmail.com>
2024-09-06 22:01:22 -0400

    minor changes

--- src/app/api/leader/route.ts
+++ src/app/api/leader/route.ts
@@ -38,6 +38,7 @@ export async function POST(req: NextRequest) {
 
     await redis.ephemeral.set(`qr:${boothId}`, JSON.stringify(qrData), 'EX', 10)
 
+    console.log(payload)
     const qr = `${process.env.URL}/api/visit?qr=${encodeURIComponent(payload)}&boothId=${boothId}`
 
     return NextResponse.json({ qr })
