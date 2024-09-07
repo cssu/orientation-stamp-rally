b6233dc9ff350ba73a48ca5a418f8f243b3f9e39
Barış <bayazitbaris@gmail.com>
2024-09-07 00:34:16 -0400

    Minor changes

--- src/app/api/get-accessed-booths/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server'
-
-const fs = require('fs')
-const file = require('@/data/booths_attended.json')
-
-export function GET(req: NextRequest) {
-    return NextResponse.json(file)
-}
--- src/app/api/leader/route.ts
+++ src/app/api/leader/route.ts
@@ -29,11 +29,12 @@ export async function POST(req: NextRequest) {
                 }
             }
         })
+        // TODO: Right now, every organization has only one booth. This may change in the future.
         .then((org) => org!.organization!.booths[0]!.boothId)
 
     const qrData = {
         payload,
-        timestamp,
+        timestamp
     }
 
     await redis.ephemeral.set(`qr:${boothId}`, JSON.stringify(qrData), 'EX', 10)
@@ -43,56 +44,3 @@ export async function POST(req: NextRequest) {
 
     return NextResponse.json({ qr })
 }
-// import { calculateHMAC } from "@/lib/utils";
-
-// /*
-// Each boothID maps to the last booth refresh and the last QR
-// For example, if boothInfo[5] = {lastRefresh: 4206942069, qr: "dfuysfadiuty"}
-// then that means that booth 5 last refreshed at the at time and had that QR
-// code during the last refresh
-// */
-// interface boothDetails {
-//   lastRefresh: number;
-//   qr: string;
-// }
-// let boothInfo: { [boothID: string]: boothDetails } = {};
-
-// const REFRESH_TIME = 15000;
-
-// export async function GET(req: NextRequest) {
-//   const { searchParams } = new URL(req.url || "");
-//   const boothID: string = searchParams.get("boothid") || "";
-//   const curTime = Date.now();
-
-//   if (!boothID) {
-//     return new Response(JSON.stringify(boothInfo[boothID]), {
-//       status: 200,
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//     });
-//   }
-
-//   if (
-//     !boothInfo[boothID] ||
-//     curTime - boothInfo[boothID].lastRefresh > REFRESH_TIME
-//   ) {
-//     genNewQR(boothID, curTime).then(
-//       (newQR) => (boothInfo[boothID] = { lastRefresh: curTime, qr: newQR })
-//     );
-//   }
-
-//   return new Response(JSON.stringify(boothInfo[boothID]), {
-//     status: 200,
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//   });
-// }
-
-// async function genNewQR(boothID: string, curTime: number): Promise<string> {
-//   const payload = `${curTime}-${boothID}`;
-//   const signature = await calculateHMAC(payload, "12272005");
-//   const qr = `${payload}-${signature}`;
-//   return qr;
-// }
